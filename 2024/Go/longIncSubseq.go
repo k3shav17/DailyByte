e0436a56b6f2a775692e2f7819e94a3c829f5d4f
Keshava Kommaraju <keshavrao0489@gmail.com>
2024-07-30 23:30:37 +0530

    day two

--- 2024/Go/longIncSubseq.go
+++ 2024/Go/longIncSubseq.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
-func main() {
+type DayOne struct{}
 
-  nums := []int{10,9,2,5,3,7,101,18}
-  dp := make([]int, len(nums))
+func (one *DayOne) LongestSubSequence() {
 
-  for i := 1; i < len(nums); i++ {
+	nums := []int{10, 9, 2, 5, 3, 7, 101, 18}
+	dp := make([]int, len(nums))
 
-    for j := 0; j < i; j++ {
+	for i := 1; i < len(nums); i++ {
 
-      if nums[i] > nums[j] {
-        dp[i] = max(dp[i], dp[j] + 1) 
-      }
-    }
-    
-  }
+		for j := 0; j < i; j++ {
 
-  max := dp[0];
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
 
-  for _, el := range dp {
-    if el > max {
-      max = el
-    }
-  }
+	}
 
-  // +1 as every single element itself forms a strictly increasing subsequence of length 1.
-  fmt.Println(max + 1)
+	max := dp[0]
+
+	for _, el := range dp {
+		if el > max {
+			max = el
+		}
+	}
+
+	// +1 as every single element itself forms a strictly increasing subsequence of length 1.
+	fmt.Println("Day one problem -> ", max+1)
 
 }
 
 func max(first, second int) int {
 
-  if first > second {
-    return first
-  }
+	if first > second {
+		return first
+	}
 
-  return second
+	return second
 }
